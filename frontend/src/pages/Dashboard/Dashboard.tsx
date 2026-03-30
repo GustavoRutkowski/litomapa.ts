7bb8b5f715704a054378877e1f34735d8a3815bc
GustavoRutkowski <140657464+GustavoRutkowski@users.noreply.github.com>
2026-03-30 17:27:01 +0000

    Implement the Header in Dashboard page

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,13 @@
+import Header from '../../components/Header/Header';
 import styles from './Dashboard.module.scss';
 
 export default function Dashboard() {
     return (
         <div className={styles.container}>
-            <header>LitoMapa</header>
-            <p>Está é a página de dashboard!</p>
+            <Header />
+            <main>
+                <p>Está é a página de dashboard!</p>
+            </main>
         </div>
     );
 }
