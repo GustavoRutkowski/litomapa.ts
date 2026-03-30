5bdc218d81d1183afd3d74d3fb0e5d37bbad6aac
GustavoRutkowski <140657464+GustavoRutkowski@users.noreply.github.com>
2026-03-30 00:45:56 +0000

    Fix the useUsers hook that depends on useAuth and generate a call cycle

--- frontend/src/hooks/useUsers.tsx
+++ frontend/src/hooks/useUsers.tsx
@@ -3,15 +3,12 @@ import {
     IUserDTO, createUser as create,
     loginUser, getUser as get
 } from '../services/users.service';
-import useAuth from './useAuth';
 
-export default function useUsers() {
-    const { token } = useAuth();
-    
+export default function useUsers() {   
     const createUser = useCallback(async (body: IUserDTO) => await create(body), []);
     const login = useCallback(async (body: IUserDTO) => await loginUser(body), []);
 
-    const getUser = useCallback(async () => {
+    const getUser = useCallback(async (token: string) => {
         if (!token) throw new Error('No authentication token available');
         return await get(token);
     }, []);
