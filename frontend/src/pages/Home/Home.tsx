b46a29082b86c3a456d53a94bcc1b9b29bc265c5
GustavoRutkowski <140657464+GustavoRutkowski@users.noreply.github.com>
2026-03-31 17:48:44 +0000

    Standardizing prototype pages

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import styles from './Home.module.scss';
 
 export default function Home() {
     return (
         <div className={styles.container}>
-            <header>LitoMapa</header>
-            <p>Está é a página principal!</p>
+            <p>Está é a página principal! <b>(em desenvolvimento)</b></p><br />
+            <p>Vá para a página de <Link to="/login">login</Link> ou para a página de <Link to="/register">registro</Link>.</p>
         </div>
     );
 }
