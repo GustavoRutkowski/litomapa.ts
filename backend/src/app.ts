1b379a995dc28e9a7397159d6e39f37c9075dc01
GustavoRutkowski <140657464+GustavoRutkowski@users.noreply.github.com>
2026-04-11 21:56:06 +0000

    Create Swagger setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,24 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import usersRouter from './routers/users.routes.js';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './swagger.js';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.use(json());
 app.use(cors({ methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'] }));
+
+app.get('/', (req, res) => res.redirect('/docs'));
+
 // Routers:
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/uploads', express.static(join(__dirname, 'uploads')));
 app.use('/users', usersRouter);
 
 // Server:
