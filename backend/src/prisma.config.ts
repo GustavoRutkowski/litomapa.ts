2838bec6dce68dd1548d259c8a639206fc6b1254
GustavoRutkowski <140657464+GustavoRutkowski@users.noreply.github.com>
2026-05-07 02:37:09 +0000

    Create prisma singleton

--- backend/src/prisma.config.ts
+++ backend/src/prisma.config.ts
@@ -2,8 +2,6 @@ import { config } from 'dotenv';
 import { defineConfig } from 'prisma/config';
 config({ path: '../../.env' });
 
-console.warn(process.env.DATABASE_URL, 'DATABASE_URL');
-
 export default defineConfig({
     schema: 'prisma/schema.prisma',
     migrations: {
